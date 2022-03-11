fbeb08f1ec367f46e6631b8478f77807eae27367
Timoteo stifft <timoteostifft@gmail.com>
2022-03-11 15:53:22 -0300

    feature: set test controller base methods

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,8 +11,29 @@ class TestController extends Controller
     protected $service;
 
     public function __construct(TestService $service){
-        $this->service = $service;
+      $this->service = $service;
     }
 
-    public function list(){}
+    public function list()
+    {
+      try{
+        $testsList = $this->service->list();
+        return response()->json($testsList,Response::HTTP_OK);
+      } catch (\Throwable$error){
+        return response()->json("Erro ao listar provas. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
+    }
+
+    public function register(Request $request)
+    {
+      try{
+        $test = $this->service->register($request->all());
+        return response()->json($test,Response::HTTP_OK);
+      } catch (\Throwable$error){
+        return response()->json("Erro ao cadastrar prova. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
+    }
+
+
+    public function remove(){}
 }
