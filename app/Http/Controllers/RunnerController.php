9dd70c5167b0f2a973c014e1de24189798d144a1
Timoteo stifft <timoteostifft@gmail.com>
2022-03-10 23:46:15 -0300

    feature: set list, register and remove methods for runner controller

--- app/Http/Controllers/RunnerController.php
+++ app/Http/Controllers/RunnerController.php
@@ -2,38 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\HttpFoundation\Response;
+
 use App\Services\RunnerService;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class RunnerController extends Controller
 {
     protected $service;
 
-    public function __construct(RunnerService $service){
+    public function __construct(RunnerService $service)
+    {
         $this->service = $service;
     }
 
-    public function list(Request $request){
+    public function list(Request $request)
+    {
         try{
-            return response()->json([
-                $this->service->list()
-            ],201);
+            $runnersList = $this->service->list();
+            return response()->json($runnersList,Response::HTTP_OK);
         } catch (\Throwable$error){
-            return response()->json(["Erro ao listar corredores. Erro: {$error}"],500);
+            return response()->json("Erro ao listar corredores. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function register(Request $request){
-
-        $data = 'request';
-
+    public function register(Request $request)
+    {
         try{
-            return response()->json([
-                $this->service->register($data)
-            ],201);
+            $runner = $this->service->register($request->all());
+            return response()->json($runner,Response::HTTP_OK);
         } catch (\Throwable$error){
-            return response()->json(["Erro ao cadastrar corredor. Erro: {$error}"],500);
+            return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function remove(Request $request)
+    {
+        try{
+            $deleted = $this->service->remove($request->route('id'));
+            return response()->json($deleted,Response::HTTP_OK);
+        } catch (\Throwable$error){
+            return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
