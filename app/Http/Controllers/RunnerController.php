16925127f3f4540a77817c4fb3637b1e9ae097cf
Timoteo stifft <timoteostifft@gmail.com>
2022-03-11 15:52:55 -0300

    feature: set test controller base methods

--- app/Http/Controllers/RunnerController.php
+++ app/Http/Controllers/RunnerController.php
@@ -9,40 +9,40 @@ use Illuminate\Http\Request;
 
 class RunnerController extends Controller
 {
-    protected $service;
+  protected $service;
 
-    public function __construct(RunnerService $service)
-    {
-        $this->service = $service;
-    }
+  public function __construct(RunnerService $service)
+  {
+      $this->service = $service;
+  }
 
-    public function list(Request $request)
-    {
-        try{
-            $runnersList = $this->service->list();
-            return response()->json($runnersList,Response::HTTP_OK);
-        } catch (\Throwable$error){
-            return response()->json("Erro ao listar corredores. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+  public function list(Request $request)
+  {
+    try{
+      $runnersList = $this->service->list();
+      return response()->json($runnersList,Response::HTTP_OK);
+    } catch (\Throwable$error){
+      return response()->json("Erro ao listar corredores. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+  }
 
-    public function register(Request $request)
-    {
-        try{
-            $runner = $this->service->register($request->all());
-            return response()->json($runner,Response::HTTP_OK);
-        } catch (\Throwable$error){
-            return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+  public function register(Request $request)
+  {
+    try{
+      $runner = $this->service->register($request->all());
+      return response()->json($runner,Response::HTTP_OK);
+    } catch (\Throwable$error){
+      return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+  }
 
-    public function remove(Request $request)
-    {
-        try{
-            $deleted = $this->service->remove($request->route('id'));
-            return response()->json($deleted,Response::HTTP_OK);
-        } catch (\Throwable$error){
-            return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+  public function remove(Request $request)
+  {
+    try{
+      $deleted = $this->service->remove($request->route('id'));
+      return response()->json($deleted,Response::HTTP_OK);
+    } catch (\Throwable$error){
+      return response()->json("Erro ao cadastrar corredor. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+  }
 }
