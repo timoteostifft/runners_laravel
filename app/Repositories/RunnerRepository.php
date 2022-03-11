ab76a2ef2286f14b4bc11d8f404eeb9f22162898
Timoteo stifft <timoteostifft@gmail.com>
2022-03-10 23:47:34 -0300

    feature: set runner repository main methods

--- app/Repositories/RunnerRepository.php
+++ app/Repositories/RunnerRepository.php
@@ -3,18 +3,28 @@
 namespace App\Repositories;
 use App\Models\Runner;
 
-class RunnerRepository {
+class RunnerRepository 
+{
   protected $model;
 
-  public function __construct(Runner $model){
+  public function __construct(Runner $model)
+  {
     $this->model = $model;
   }
 
-  public function list(){
-    $this->model->list();
+  public function list()
+  {
+    return $this->model->get();
   }
 
-  public function register(mixed $data){
-    $this->model->register($data);
+  public function register(array $data)
+  {
+    return $this->model->create($data);
+  }
+
+  public function remove($id){
+    $sql = $this->model->where('id',$id)->get();
+    $this->model->remove($id);
+    return $sql;
   }
 }
