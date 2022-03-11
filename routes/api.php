2fae26c616d3d3e66e54a3725ac778b8af0a65e3
Timoteo stifft <timoteostifft@gmail.com>
2022-03-11 14:56:40 -0300

    feature: set test base routes

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/home','App\Http\Controllers\RunnerController@list');
 Route::post('/home','App\Http\Controllers\RunnerController@register');
 Route::delete('/home/{id}', 'App\Http\Controllers\RunnerController@remove');
+
+//tests routes
+Route::get('/home/tests','App\Http\Controllers\TestController@list');
+Route::post('/home/tests','App\Http\Controllers\TestController@register');
+Route::delete('/home/test/{id}', 'App\Http\Controllers\TestController@remove');
