a656e88c161bc9a720565e4b7ec7514a8fadf97f
Timoteo stifft <timoteostifft@gmail.com>
2022-03-14 01:29:15 -0300

    feature: set register runner on test method for runnes tests controller

--- app/Http/Controllers/RunnerTestController.php
+++ app/Http/Controllers/RunnerTestController.php
@@ -15,19 +15,24 @@ class RunnerTestController extends Controller
       $this->service = $service;
   }
 
-  public function list(Request $request)
+  public function list(Request $request, $testId, $runnerId)
   {
     try{
-      $runnersTestsList = $this->service->list($request->all());
+      $runnersTestsList = $this->service->list($testId, $runnerId);
       return response()->json($runnersTestsList,Response::HTTP_OK);
     } catch (\Throwable$error){
-      return response()->json("Erro ao listar corredores. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
+      return response()->json("Erro ao listar inscritos em provas. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }
 
-  public function register(Request $request)
+  public function register(Request $request, $testId, $runnerId)
   {
-
+    try{
+      $runnersTestsRegistered = $this->service->register($testId, $runnerId);
+      return response()->json($runnersTestsRegistered,Response::HTTP_OK);
+    } catch (\Throwable$error){
+      return response()->json("Erro ao inscrever corredor em prova. Erro: {$error}",Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
   }
 
   public function remove($testId, $runenrId)
