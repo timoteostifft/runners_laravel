5f0d5de41ea80e18ff080ae26df58c72ee6af6c6
Timoteo stifft <timoteostifft@gmail.com>
2022-03-14 01:28:40 -0300

    feature: set list and register methods for runners tests repository

--- app/Repositories/RunnerTestRepository.php
+++ app/Repositories/RunnerTestRepository.php
@@ -12,14 +12,17 @@ class RunnerTestRepository
     $this->model = $model;
   }
 
-  public function list()
+  public function list($testId, $runnerId)
   {
-    return $this->model->get();
+    return $this->model->where('test_id','=',$testId)->where('runner_id','=',$runnerId)->get();
   }
 
-  public function register(array $data)
+  public function register($testId, $runnerId)
   {
-    return $this->model->create($data);
+    return $this->model->create([
+      'test_id' => $testId,
+      'runner_id' => $runnerId
+    ]);
   }
 
   public function remove($id){
