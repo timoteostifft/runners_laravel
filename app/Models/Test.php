ec2912a70cc24df6d8e75968969c4c517c96dd93
Timoteo stifft <timoteostifft@gmail.com>
2022-03-14 14:28:04 -0300

    feature:  set getRunners method

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 class Test extends Model
 {
   public $timestamps = false;
@@ -12,9 +14,28 @@ class Test extends Model
   protected $fillable = [
     'type',
     'date',
+    'start',
+    'finish'
   ];
 
-  public function remove($id){
+  public function remove($id)
+  {
     self::where('id', $id)->delete();
+  }
+
+  public function getRunners($testId)
+  {
+    
+    $sql = DB::select("select runners.id, runners.name, runners.cpf, runners.birth from runners, runners_tests where runners.id = runners_tests.runner_id and runners_tests.test_id = ?",[$testId]);
+    
+    $runnersArray = array();
+
+    foreach($sql as $object)
+    {
+      
+      array_push($runnersArray, $object);
+    }
+
+    return $sql;
   }
 }         
