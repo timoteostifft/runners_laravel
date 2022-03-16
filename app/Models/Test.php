4afdde95aa2cf6c2ba3840dedbf50b7233c5b212
Timoteo stifft <timoteostifft@gmail.com>
2022-03-16 19:17:52 -0300

    chore: update query to clean code methods

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -26,7 +26,22 @@ class Test extends Model
   public function getRunnersByAge($testId)
   {
     
-    $sql = DB::select("select runners.id, runners.name, runners.cpf, runners.birth, DATE_FORMAT(FROM_DAYS(DATEDIFF(now(), birth)), '%Y')+0 AS age from runners, runners_tests where runners.id = runners_tests.runner_id and runners_tests.test_id = ? order by age",[$testId]);
+  $sql = DB::select("SELECT 
+                      runners.*,
+                      DATE_FORMAT(FROM_DAYS(DATEDIFF(now(), runners.birth)), '%Y')+0 AS age,
+                      runners_tests.id as 'runner_tests_id',
+                      runners_tests.test_id
+                    FROM 
+                      runners
+                    JOIN	
+                      runners_tests on 
+                      (
+                        runners.id = runners_tests.runner_id
+                      )
+                    WHERE 
+                      runners_tests.test_id  = ?
+                    ORDER BY
+                      birth DESC",[$testId]);
     
     //old list method
     //sql = DB::select("select runners.id, name, birth, cpf, DATE_FORMAT(FROM_DAYS(DATEDIFF(now(), birth)), '%Y')+0 AS age from runners, runners_tests where runners_tests.test_id = ? order by age",['1']);
@@ -39,5 +54,12 @@ class Test extends Model
     }
 
     return $sql;
+  }
+
+  public function getRunnersByResult($testId)
+  {
+    $sql = DB::select('select runners.id, runners.name, runners.cpf, runners.birth, TIMEDIFF(runners_tests.finish, runners_tests.kickoff) AS time from runners, runners_tests where runners.id = runners_tests.runner_id and runners_tests.test_id = ? order by time',[$testId]);
+
+    return($sql);
   }
 }         
