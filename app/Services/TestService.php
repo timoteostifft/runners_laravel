ad2966fb50b55f6823c5a33aa6804bad01b72612
Timoteo stifft <timoteostifft@gmail.com>
2022-03-18 10:54:25 -0300

    feature: set list by age groups method on test service

--- app/Services/TestService.php
+++ app/Services/TestService.php
@@ -14,13 +14,46 @@ class TestService{
 
   public function listByAge()
   {
-    $data =  $this->testRepository->list();
-    foreach ($data as $test){
-      $runners = $this->testRepository->getRunnersByAge($test->id);
-      $test->runners = $runners;
-    }
+    $testsList =  $this->testRepository->list();
 
-    return($data);
+    foreach ($testsList as $test)
+    {
+      $runners = $this->testRepository->getRunnersByAge($test->id);
+
+      $youngerThan25 = (array) null;
+      $youngerThan35 = (array) null;
+      $youngerThan45 = (array) null;
+      $youngerThan55 = (array) null;
+      $olderThan55 = (array) null;
+
+      foreach($runners as $runner)
+      {        
+        if($runner->age <= 25)
+        {
+          array_push($youngerThan25, $runner);
+        } else {
+          if($runner->age <= 35){
+            array_push($youngerThan35, $runner);
+          } else {
+            if($runner->age <= 45){
+              array_push($youngerThan45, $runner);
+            } else {
+              if ($runner->age <= 55){
+                array_push($youngerThan55, $runner);
+              } else {
+                array_push($olderThan55, $runner);
+              }
+            } 
+          }
+        }
+      }
+      $test->youngerThan25 = $youngerThan25;
+      $test->youngerThan35 = $youngerThan35;
+      $test->youngerThan45 = $youngerThan45;
+      $test->youngerThan55 = $youngerThan55;
+      $test->olderThan55 = $olderThan55;
+    }
+    return($testsList);
   }
 
   public function listByResult()
